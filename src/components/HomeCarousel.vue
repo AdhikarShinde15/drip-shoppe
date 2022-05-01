58ea6d56ae02b3672db255372597cd52a7d08e33
AdhikarShinde <adhikar.shinde@coditas.com>
2022-05-01 12:08:02 +0530

    added carsoule

---
+++ src/components/HomeCarousel.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+     <VueSlickCarousel >
+      <img src="../assets/homeCar.svg" alt="">
+      <img src="../assets/homeCar.svg" alt="">
+      <img src="../assets/homeCar.svg" alt="">
+      <img src="../assets/homeCar.svg" alt="">
+    </VueSlickCarousel>
+  </div>
+</template>
+
+<script>
+import VueSlickCarousel from "vue-slick-carousel";
+import "vue-slick-carousel/dist/vue-slick-carousel.css";
+import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
+
+export default {
+  components: { VueSlickCarousel },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,43 @@
 <template>
   <div>
-   Landing Page
+    <Carousel />
+    <div class="home-flex">
+      <span class="left"> Shop The Latest </span>
+      <span class="right"> View All </span>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+import Carousel from "../components/HomeCarousel.vue";
+export default {
+  components: {
+    Carousel,
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+.home-flex {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.left {
+  font-family: "DM Sans";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 33px;
+  line-height: 43px;
+  color: #000000;
+}
+.right {
+  font-family: "DM Sans";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 26px;
+  text-transform: capitalize;
+  color: #a18a68;
+}
 </style>
